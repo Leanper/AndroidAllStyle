32dd9bde81ce1f1edf534d95f5b3ea357cc0b954
Leanper <15116920121@163.com>
2019-02-18 17:15:06 +0800

    添加桌面快捷方式

--- app/src/main/java/com/leanper/androidallstyle/activity/SendShortcutActivity.java
+++ app/src/main/java/com/leanper/androidallstyle/activity/SendShortcutActivity.java
@@ -1,18 +1,10 @@
 package com.leanper.androidallstyle.activity;
 
-import android.app.Activity;
-import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ShortcutInfo;
-import android.content.pm.ShortcutManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Icon;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 
 import com.leanper.androidallstyle.R;
@@ -29,51 +21,51 @@ public class SendShortcutActivity extends AppCompatActivity {
         findViewById(R.id.button3s).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.icon_three);
+                addShortCut();
+            }
+        });
 
-                addShortcut(SendShortcutActivity.this,"sss",bitmap,new Intent());
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteShortCut(SendShortcutActivity.this);
             }
         });
     }
 
-    public void addShortcut(Activity activity, String name, Bitmap icon, Intent actionIntent) {
-
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
-            //  创建快捷方式的intent广播
-            Intent shortcut = new Intent("com.android.launcher.action.INSTALL_SHORTCUT");
-            // 添加快捷名称
-            shortcut.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
-            //  快捷图标是允许重复(不一定有效)
-            shortcut.putExtra("duplicate", false);
-            // 快捷图标
-            // 使用资源id方式
-//            Intent.ShortcutIconResource iconRes = Intent.ShortcutIconResource.fromContext(activity, R.mipmap.icon);
-//            shortcut.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconRes);
-            // 使用Bitmap对象模式
-            shortcut.putExtra(Intent.EXTRA_SHORTCUT_ICON, icon);
-            // 添加携带的下次启动要用的Intent信息
-            shortcut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, actionIntent);
-            // 发送广播
-            activity.sendBroadcast(shortcut);
-        } else {
-            ShortcutManager shortcutManager = (ShortcutManager) activity.getSystemService(Context.SHORTCUT_SERVICE);
-            if (null == shortcutManager) {
-                // 创建快捷方式失败
-                Log.e("MainActivity", "Create shortcut failed");
-                return;
-            }
-            ShortcutInfo shortcutInfo = new ShortcutInfo.Builder(activity, name)
-                    .setShortLabel(name)
-                    .setIcon(Icon.createWithBitmap(icon))
-                    .setIntent(actionIntent)
-                    .setLongLabel(name)
-                    .build();
-            shortcutManager.requestPinShortcut(shortcutInfo, PendingIntent.getActivity(activity,
-                    RC_CREATE_SHORTCUT, actionIntent, PendingIntent.FLAG_UPDATE_CURRENT).getIntentSender());
-        }
+    public void addShortCut(){
+        // 安装的Intent
+        Intent shortcut = new Intent("com.android.launcher.action.INSTALL_SHORTCUT");
+        // 快捷名称
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_NAME, "mm");
+        // 快捷图标是否允许重复
+        shortcut.putExtra("duplicate", false);
+        Intent shortcutIntent = new Intent(Intent.ACTION_MAIN);
+        shortcutIntent.putExtra("参数名", "参数值");
+        //可以不传com.leanper.androidallstyle
+        shortcutIntent.setClassName(getPackageName(), ".activity.SendShortcutActivity");
+        shortcutIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+        // 快捷图标 TODO 图标处理 将网络图片下载并创建快捷方式图标
+        Intent.ShortcutIconResource iconRes = Intent.ShortcutIconResource.fromContext(SendShortcutActivity.this, R.mipmap.icon_three);
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconRes);
+        // 发送广播
+        sendBroadcast(shortcut);
 
     }
 
+    //删除快捷方式
+    private void deleteShortCut(Context mContext) {
+        Intent intent = new Intent("com.android.launcher.action.UNINSTALL_SHORTCUT" );
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, "mm");
+        // 要删除的应用程序的ComponentName，即应用程序包名+activity的名字
+        ComponentName comp = new ComponentName("com.leanper.androidallstyle",
+                "com.leanper.androidallstyle.activity.SendShortcutActivity");
+        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, new Intent()
+                .setComponent(comp).setAction("android.intent.action.MAIN"));
+        sendBroadcast(intent);
+
+    }
 
 
 }
